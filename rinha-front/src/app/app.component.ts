f1fdc931404a93daccf1062a3a2b9977ac924f6d
guilhermedel <glhrmgithub@gmail.com>
2023-10-24 21:59:08 -0300

    creating read json

--- rinha-front/src/app/app.component.ts
+++ rinha-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import fs from 'fs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rinha-front';
+  
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      this.processJSONFile(selectedFile);
+    }
+  }
+
+  async processJSONFile(file:string){
+    try {
+      const rawData = fs.readFileSync(file, 'utf-8');
+      const jsonData = JSON.parse(rawData);
+      const totalItems = jsonData.length;
+  
+      for (let i = 0; i < totalItems; i += batchSize) {
+        const batch = jsonData.slice(i, i + batchSize);
+        //await processBatch(batch);
+      }
+  
+      console.log('Processamento concluído.');
+    } catch (error) {
+      console.error('Erro durante o processamento:', error);
+    }
+  }
+
+
 }
